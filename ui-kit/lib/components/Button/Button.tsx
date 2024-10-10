bad9134a73d1501e7a864debafd0bc318b89425a
German <germanolle@gmail.com>
2024-10-10 18:25:19 -0300

    initial commit

---
+++ ui-kit/lib/components/Button/Button.tsx
@@ -0,0 +1,6 @@
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+export const Button = ({ children, ...props}: ButtonProps) => {
+  return <button {...props}>
+    {children}
+  </button>
+}
---
+++ ui-kit/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: resolve(__dirname, "tsconfig.lib.json"),
+    }),
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime']
+    }
+  }
+})
