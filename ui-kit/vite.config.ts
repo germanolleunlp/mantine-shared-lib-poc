d3e68d44efbe48a471d0b6ba34843fa04a729339
German <germanolle@gmail.com>
2024-10-10 18:38:03 -0300

    Add more vite config settings

--- ui-kit/vite.config.ts
+++ ui-kit/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import { extname, relative, resolve } from 'path'
+import { fileURLToPath } from 'node:url'
+import { glob } from 'glob'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,7 +20,22 @@ export default defineConfig({
       formats: ['es']
     },
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime']
+      external: ['react', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        glob.sync('lib/**/*.{ts,tsx}', {
+          ignore: ["lib/**/*.d.ts"],
+        }).map(file => [
+          relative(
+            'lib',
+            file.slice(0, file.length - extname(file).length)
+          ),
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      }
     }
   }
 })
